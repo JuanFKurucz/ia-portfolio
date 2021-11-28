980b2269e1dbd84d2273e67a605dc1880a3d7b9f
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2021-11-28 19:03:27 -0300

    Updates

--- component---src-pages-tags-jsx-36d728d4bddd2cc88563.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{PcuZ:function(t,e,a){"use strict";a.r(e);a("q1tI");var n=a("tCqT"),c=a("TiLl"),u=a("qKvR");e.default=function(t){var e=t.pageContext.tags;return Object(u.d)(n.d,null,Object(u.d)(c.b,{title:"Tags Page"},"Gatsby Tutorial Starter"),Object(u.d)(n.a,null,Object(u.d)(c.e,{list:e})))}}}]);
-//# sourceMappingURL=component---src-pages-tags-jsx-36d728d4bddd2cc88563.js.map
